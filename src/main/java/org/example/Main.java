e736cdd04363d00faa688a8f7cca8e8a8fa55a3f
Vitta Khabaev <Vittahabaev@qmail.com>
2023-06-08 22:55:22 +0600

    ster ingushetiyu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Commit 1");
         System.out.println("salam aleykum Chechnya");
-        System.out.println("salam aleykum brat`yam iz Ingushetii");
+        System.out.println("salam aleykum");
     }
 }
